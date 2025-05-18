948dd69f3e64f4293266b1e44ffb6fa544f20fb9
TomHTML <nguyendinhtandatdiscord@gmail.com>
2025-05-19 05:31:28 +0700

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ alert("PLEASE READ THIS: Before going into this website, click okay to continue"
 alert("To make sure that you're safe, I'll introduce to the person who made this website, he's Nguyen Dinh Tan Dat, your classmates right?")
 alert("You see? This is not a virus")
 alert("Now we proceed to the website... Thank you for reading this :D")
-let heading = document.getElementById()
+let heading = document.getElementById("heading")
 let duration = 0.3
 let label = document.getElementById("hovering_open")
 label.style.opacity = "0";
